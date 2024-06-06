982c6bb9b7c1d1c4eee50e076271e60d06101e11
rafael_nunes <rafa.nuns17@gmail.com>
2024-06-06 16:25:45 -0300

    DepartmentList view design

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,19 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class DepartmentListController implements Initializable{
+
+	@FXML
+	private Button buttonNovo;
+	
+	@Override
+	public void initialize(URL url, ResourceBundle eb) {
+		
+	}
+}
